d4680cd77b1d729eb2b75daa7ab6fae8f1b53d13
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-02-29 15:34:59 +0000

    made test work

--- ECommerceTests/BasketTest.cs
+++ ECommerceTests/BasketTest.cs
@@ -12,15 +12,16 @@ namespace ECommerceTests
         public void Test_addItem_AddsAnItem_WhenAnItemIsAdded()
         {
             //Arrange
+            string name = "PocketWormHole";
             Basket basket = new Basket();
             Item item1 = new Item();
-            item1.itemName = "HelicopterHat";
+            item1.itemName = name;
             
             //Act
             Dictionary<int,Item> basket1 = basket.addItem(item1);
 
             //Assert
-            Assert.AreEqual("HelicopterHat", basket1[0]);
+            Assert.AreEqual(name, basket1[0].itemName);
         }
     }
 }
