71d99474cf6b65385c43daba14f5d5c92f99dc1b
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-03-02 17:04:48 +0000

    Added database getitems method

---
+++ DatabaseLibrary/Dump.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLibrary
+{
+    public class Dump
+    {
+        public List<item> GetItems()
+        {
+            List<item> itemNameList = new List<item>();
+
+            using (var context = new ECommerceProjectSystemEntities())
+            {
+                foreach (var item in context.items)
+                {
+                    itemNameList.Add(item);
+                }
+            }
+            return itemNameList;
+        }
+    }
+}
--- ECommerce.Project/Program.cs
+++ ECommerce.Project/Program.cs
@@ -1,4 +1,5 @@
-﻿using log4net;
+﻿using DatabaseLibrary;
+using log4net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +16,23 @@ namespace ECommerce.Project
 
         static void Main(string[] args)
         {
+            Dump dump = new Dump();
+
+            List<item> itemDumpList = dump.GetItems();
+
+            foreach (var item in itemDumpList)
+            {
+                Console.WriteLine(item.item_name);
+            }
+            Console.ReadLine();
+
+            /*
             //Testing the logger function.
             string message = "Hello";
             Console.WriteLine(message);
             logger.Info(message);
             Console.ReadLine();
+            */
         }
     }
 }
