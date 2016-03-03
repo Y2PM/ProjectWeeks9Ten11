e4356f7ecae6bba58c28dab6092c3ee00b82ead6
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-03-03 16:25:11 +0000

    Edited wpf button a bit, working towards using an event to log.

--- WpfECommerceProject/MainWindow.xaml.cs
+++ WpfECommerceProject/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfECommerceProject
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BuyButton.Content = "Buy button was clicked";
+        }
     }
 }
