95ba444b28a9804af48f3c27ed31a824766db293
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-03-14 18:52:32 +0000

    trying to finish service

--- WCFServiceCL/GetPriceService.cs
+++ WCFServiceCL/GetPriceService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatabaseLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,20 @@ namespace WCFService
     public class GetPriceService : IGetPriceService
     {
         GetPrice thePriceO = new GetPrice();
-        
+        //Dump dump = new Dump(new ECommerceProjectSystemEntities());
 
         public int GetPrice()
         {
+
+
+            //List<item> items = dump.GetItemsFromDB();
+            //decimal totprice;
+
+            //foreach (item iteM in items)
+            //{
+            //    totprice += (decimal)iteM.item_price;
+            //}
+
             thePriceO.thePrice = 10;//Here can get data from database / entity frame work maybe.
             return thePriceO.thePrice;
         }
